9d2b286eff018792ba599416db9e38862565b47e
magnum2334 <kevincallem19@gmail.com>
2024-12-07 16:17:26 -0500

    add: DTO ruta

--- src/ruta/dto/create-ruta.dto.ts
+++ src/ruta/dto/create-ruta.dto.ts
@@ -1,1 +1,24 @@
-export class CreateRutaDto {}
+import { IsString, IsInt, IsBoolean, IsDateString } from 'class-validator';
+
+export class CreateRutaDto {
+  @IsString()
+  nombre: string;
+
+  @IsInt()
+  cobradorId: number;
+
+  @IsInt()
+  interes: number;
+
+  @IsInt()
+  tMaximoPrestamo: number;
+
+  @IsBoolean()
+  interesLibre: boolean;
+
+  @IsDateString()
+  fecha_creacion: string;
+
+  @IsInt()
+  capitalId: number;
+}
